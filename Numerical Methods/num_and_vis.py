0ab53a2c430c45679718be4972e1930056252490
dleahy27 <97338609+dleahy27@users.noreply.github.com>
2023-01-24 12:02:53 +0000

    Update num_and_vis.py

--- Numerical Methods/num_and_vis.py
+++ Numerical Methods/num_and_vis.py
@@ -244,7 +244,6 @@ def integrate_2d(func, x_vals,y_vals):
 
     return val
 
-# FCTS method to solve one-dimensional case numerically (cases A & C)
 def ftcs_1D(case, settings, sys_par, num_par):
     
     # set up grids
@@ -292,13 +291,8 @@ def ftcs_1D(case, settings, sys_par, num_par):
      
     # run loop to compute FTCS scheme and write mod square of result to file
     for i in np.arange(tn):
-        
-        """ 
-        fix this and include 
-        potential term!
-        """
-        
-        psi[1:xn-1] = psi[1:xn-1] + dt*1j*(psi[2:xn]-2*psi[1:xn-1]+psi[0:xn-2])/(2*dx**2) 
+
+        psi[1:xn-1] = psi[1:xn-1] + dt*1j*((psi[2:xn]-2*psi[1:xn-1]+psi[0:xn-2])/(2*dx**2) + V[1:xn-1]*psi[1:xn-1])
         psi[0] = 0
         psi[xn-1] = 0
         
@@ -367,15 +361,11 @@ def ftcs_2D(case, settings, sys_par, num_par):
     for k in np.arange(tn):
         for i in np.arange(xn-1):
             
-            """
-            fix this and include potential!
-            """
-            
-            psi[i,1:yn-1] = psi[i,1:yn-1] + dt*1j*((psi[i+1,2:yn]-2*psi[i,1:yn-1]+psi[i-1,0:yn-2])/(2*dx**2))
-            psi[:,0] = 0
-            psi[:, yn] = 0
-            psi[xn,:] = 0
-            psi[:, :] = 0
+            psi[i,1:yn-1] = psi[i,1:yn-1] + dt*1j*((psi[i+1,2:yn]-2*psi[i,1:yn-1]+psi[i-1,0:yn-2])/(2*dx**2) + V[i,1:xn-1]*psi[i,1:xn-1])
+            psi[0:,0] = 0
+            psi[0:, yn] = 0
+            psi[xn,0:] = 0
+            psi[0, 0:] = 0
             
         if (t[k] in T):
             P[j]   = np.abs(psi)**2
@@ -396,21 +386,155 @@ def ftcs_2particle(case, settings, sys_par, num_par):
 
 # RK4 method to solve one-dimensional case numerically (cases A & C)
 def rk4_1D(case, settings, sys_par, num_par):
+     
+    # set up grids
+    x_min = num_par[0]
+    x_max = num_par[1]
+    dx    = num_par[2]
     
-    """
-    to do!
-    """
+    t_start = 0
+    t_end   = sys_par[0]
+    dt      = num_par[3]
     
-    return 0
+    t = np.arange(t_start, t_end+dt, dt)
+    x = np.arange(x_min, x_max+dx, dx)
+    
+    # determine grid lengths
+    tn = len(t)
+    xn = len(x)
+    
+    # initialise wavefunction 
+    psi = wavepacket_1d(x, sys_par)
+    psi.dtype = complex 
+    
+    # set up relevant potential at each point
+    if case=="caseA":
+        V = np.zeros(xn)
+    if case=="caseC":
+        V = potential_C(x, sys_par)
+    
+    # make relevant adjustments for non-static/semi-static output:
+    if float(settings[0])==0:    
+        T   = np.arange(t_start, t_end+dt, dt*100)
+        P   = np.empty(len(T), dtype="object")
+        val = np.empty(len(T), dtype="float")
+        j       = 0
+    if float(settings[0])==0.5: 
+        T   = np.array([t_start,t_end/8,t_end/4,t_end/2, 3*t_end/4, t_end]) 
+        P   = np.empty(len(T), dtype="object")
+        val = np.empty(len(T))
+        j   = 0
+    else:
+        T   = np.array([t_end])
+        P   = np.empty(1, dtype="object")
+        val = np.array([1], dtype="float")
+        j   = 0
+     
+    #Loop over the time values and calculate the derivative
+    for i in range(1,tn):
+        f = np.zeros(xn, dtype=np.complex); k1 = np.zeros(xn,dtype=np.complex);  k2 = np.zeros(xn, dtype=np.complex); k3 = np.zeros(xn, dtype=np.complex); k4 = np.zeros(xn, dtype=np.complex)
+
+        f[1:xn-1] = (1j/2)*(psi[0:xn-2]-2*psi[1:xn-1]+psi[2:xn])/(dx**2) + V[1:xn-1]*psi[1:xn-1]
+
+        k1 = f
+
+        k2[1:xn-1] = f[1:xn-1] + (1j/4)*(k1[0:xn-2]-2*k1[1:xn-1]+k1[2:xn])/(dx**2) + V[1:xn-1]*(psi[1:xn-1] + k1[1:xn-1])
+
+        k3[1:xn-1] = f[1:xn-1] + (1j/4)*(k2[0:xn-2]-2*k2[1:xn-1]+k2[2:xn])/(dx**2) + V[1:xn-1]*(psi[1:xn-1] + k2[1:xn-1])
+
+        k4[1:xn-1] = f[1:xn-1] + (1j/2)*(k3[0:xn-2]-2*k3[1:xn-1]+k3[2:xn])/(dx**2) + V[1:xn-1]*(psi[1:xn-1] + k2[1:xn-1])
+
+        psi = psi + dt*(k1/6 + k2/3 + k3/3 + k4/6)
+
+        #Force boundary conditions on the off chance something has gone wrong and they contain a value
+        psi[0] = 0; psi[xn-1] = 0
+
+        if (t[i] in T):
+            P[j]   = np.abs(psi)**2
+            val[j] = integrate_1d(P[j],x)
+            j += 1
+
+    
+    return P, x, val, T
 
 # RK4 method to solve two-dimensional case numerically (cases B & D)
 def rk4_2D(case, settings, sys_par, num_par):
+     
+    # set up grids
+    x_min = num_par[0]
+    x_max = num_par[1]
+    dx    = num_par[2]
     
-    """
-    to do!
-    """
+    y_min = num_par[4]
+    y_max = num_par[5]
+    dy    = num_par[6]
     
-    return 0
+    t_start = 0
+    t_end   = sys_par[0]
+    dt      = num_par[3]
+    
+    t = np.arange(t_start, t_end+dt, dt)
+    x = np.arange(x_min, x_max+dx, dx)
+    y = np.arange(y_min, y_max+dy, dy)
+    
+    # determine grid lengths
+    tn = len(t)
+    xn = len(x)
+    yn = len(y)
+    
+    # initialise wavefunction 
+    psi = wavepacket_2d(x,y, sys_par)
+    psi.dtype = complex 
+    
+    # set up relevant potential at each point
+    if case=="caseB":
+        V = np.zeros(xn)
+    if case=="caseD":
+        V = potential_D(x,sys_par)
+        
+    # make relevant adjustments for non-static/semi-static output:
+    if settings[2]=="0":    
+        T   = np.arange(t_start, t_end+dt, dt*100)
+        P   = np.empty(len(T), dtype="object")
+        val = np.empty(len(T))
+        j       = 0
+    if settings[2]=="0.5": 
+        T   = np.array([t_start,t_end/8,t_end/4,t_end/2, 3*t_end/4, t_end]) 
+        P   = np.empty(len(T), dtype="object")
+        val = np.empty(len(T))
+        j   = 0
+    else:
+        T   = np.array([t_end])
+        P   = np.empty((xn,yn))
+        val = np.array([1])
+        j   = 0   
+    #Loop over the time values and calculate the derivative
+    for k in range(1,tn):
+        for i in range(1,xn):
+            f = np.zeros((xn,yn), dtype=np.complex); k1 = np.zeros((xn,yn),dtype=np.complex);  k2 = np.zeros((xn,yn), dtype=np.complex); k3 = np.zeros((xn,yn), dtype=np.complex); k4 = np.zeros((xn,yn), dtype=np.complex)
+
+            f[1:xn-1] = (1j/2)*(phi[0:xn-2]-2*phi[1:xn-1]+phi[2:xn])/(dx**2)# + V[1:xn-1]*phi[1:xn-1]
+
+            k1 = f
+
+            k2[1:xn-1] = f[1:xn-1] + (1j/4)*(k1[0:xn-2]-2*k1[1:xn-1]+k1[2:xn])/(dx**2)# + V[1:xn-1]*(phi[1:xn-1] + k1[1:xn-1])
+
+            k3[1:xn-1] = f[1:xn-1] + (1j/4)*(k2[0:xn-2]-2*k2[1:xn-1]+k2[2:xn])/(dx**2)# + V[1:xn-1]*(phi[1:xn-1] + k2[1:xn-1])
+
+            k4[1:xn-1] = f[1:xn-1] + (1j/2)*(k3[0:xn-2]-2*k3[1:xn-1]+k3[2:xn])/(dx**2)# + V[1:xn-1]*(phi[1:xn-1] + k2[1:xn-1])
+
+            phi = phi + dt*(k1/6 + k2/3 + k3/3 + k4/6)
+
+        #Force boundary conditions on the off chance something has gone wrong and they contain a value
+        phi[0] = 0; phi[xn-1] = 0
+
+        if (t[k] in T):
+            P[j]   = np.abs(psi)**2
+            val[j] = integrate_2d(P[j],x,y)
+            j += 1   
+                                                 
+    # return output                                    
+    return P, x, y, val, T
 
 # RK4 method to solve two-particle case numerically (case E)  
 def rk4_2particle(case, settings, sys_par, num_par): 
